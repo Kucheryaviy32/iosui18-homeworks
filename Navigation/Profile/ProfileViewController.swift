535c2a2f1605a9660be83c0e8058a28ca6f0054c
Игорь Скворцов <skvortsov_iv_siv@mail.ru>
2022-05-15 22:10:08 +0300

    Выполнено ДЗ 2.2

--- Navigation/Profile/ProfileViewController.swift
+++ Navigation/Profile/ProfileViewController.swift
@@ -5,43 +5,46 @@ class ProfileViewController: UIViewController {
     
     let profileHeaderView = ProfileHeaderView()
     
+    lazy var button : UIButton = {
+        let button = UIButton()
+        button.setTitle("Кнопка", for: .normal)
+        button.backgroundColor = .blue
+        return button
+    }()
+    
     override func viewDidLoad() {
         
         super.viewDidLoad()
         title = "Профиль"
-        self.view.addSubview(profileHeaderView)
+        view.addSubview(profileHeaderView)
+        view.addSubview(button)
+        
+        view.backgroundColor = .lightGray
+        profileHeaderView.translatesAutoresizingMaskIntoConstraints = false
+        button.translatesAutoresizingMaskIntoConstraints = false
 
-
+        
+        NSLayoutConstraint.activate([
+            profileHeaderView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            profileHeaderView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            profileHeaderView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            profileHeaderView.heightAnchor.constraint(equalToConstant: 220),
+            button.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            button.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            button.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            button.heightAnchor.constraint(equalToConstant: 40)
+        ])
+        profileHeaderView.backgroundColor = .systemGray5
     }
+    
     
     override func viewWillLayoutSubviews() {
         
         super.viewWillLayoutSubviews()
 
-        profileHeaderView.backgroundColor = .systemGray5
-        //выбрал не .lightGray потому что он получился очень темным
+        profileHeaderView.frame = view.frame
         
-        profileHeaderView.frame.origin = CGPoint(x: self.view.safeAreaInsets.left, y: self.view.safeAreaInsets.top)
-        profileHeaderView.frame.size = CGSize(width: view.frame.width, height: view.frame.height - self.view.safeAreaInsets.top - self.view.safeAreaInsets.bottom)
-        //для сабвью определил "безопасные" границы
-        
-        profileHeaderView.addSubview(profileHeaderView.profileImageView)
-        profileHeaderView.addSubview(profileHeaderView.userNameLabel)
-        profileHeaderView.addSubview(profileHeaderView.statusLabel)
-        profileHeaderView.addSubview(profileHeaderView.statusTextField)
-        profileHeaderView.addSubview(profileHeaderView.statusButton)
-        // добавим все элементы вью
-        
-        profileHeaderView.profileImageView.frame.origin = CGPoint(x: 16, y: 16)
-        profileHeaderView.userNameLabel.frame.origin = CGPoint(x: profileHeaderView.profileImageView.frame.width + 32, y: 27)
-        profileHeaderView.statusButton.frame.origin = CGPoint(x: 16, y: profileHeaderView.profileImageView.frame.maxY + 16 + 40)
-        profileHeaderView.statusButton.frame.size = CGSize(width: profileHeaderView.frame.width - 32, height: 50)
-        
-            // изначально прописал зависимость UILabel статуса от положения кнопки по оси Y и отнял 34, чтобы совпадало с условием задачи (без звездочки), и значение менялось от размера аватарки (так как ее размер тоже не прописан), но выполняя задачу со звездочкой невозможно в расстояние 34 вместить UITextLabel высотой 40, еще и с отступами, поэтому размеры обозначил сам
-        
-        profileHeaderView.statusTextField.frame.origin = CGPoint(x: profileHeaderView.profileImageView.frame.width + 32, y: profileHeaderView.statusButton.frame.origin.y - 34 - 40)
-        
-        profileHeaderView.statusLabel.frame.origin = CGPoint(x: profileHeaderView.profileImageView.frame.width + 32, y: profileHeaderView.statusTextField.frame.minY - 32)
+       
     }
     
     
